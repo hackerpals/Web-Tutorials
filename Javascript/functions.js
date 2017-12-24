10778e490bd1924710a5b2a52b9e585b768c772a
Hazibii <tkdparallax@gmail.com>
2017-12-24 13:18:34 +0800

    functions.js

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -5,3 +5,41 @@ var sqaure = functoin(x){
 };
 
 console.log(sqaure(12));
+
+
+var makeNoise = function(){
+  console.log("Pling!");
+};
+
+makeNoise();
+// -> Pling!
+
+var power = function(base, exponent){
+  var result = 1;
+  for(var count = 0; count < exponent; count++){
+    result *= base;
+  }
+  return result;
+}
+
+console.log(power(2,10));
+// -> 1024
+
+
+// paramaters and scoping
+
+var x = 'outside';
+
+var f1 = function(){
+  var x = "inside f1";
+};
+f1();
+console.log(x);
+// -> outside
+
+var f2 = function(){
+  x = "inside f2";
+};
+f2();
+console.log(x);
+//-> inside f2
